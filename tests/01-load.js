381e09813dd5f852c4e8e641a35b3370e6432079
mesutkurtulan <mstkrtln@gmail.com>
2021-09-20 00:22:08 +0300

    second commit

--- tests/01-load.js
+++ tests/01-load.js
@@ -1,32 +1,29 @@
 import http from 'k6/http';
-
-import { check, sleep } from 'k6';
+import { check } from 'k6';
 
 import { Rate } from 'k6/metrics';
 
 export let errorRate = new Rate('errors');
 
 export let options = {
-    stages: [
-        { duration: '5s', target: 10 },
-        //{ duration: '5s', target: 10 },
-        //{ duration: '5s', target: 10 },
-    ],
-
-    treshold : {
+    thresholds : {
         errors: ['rate<0.1'],
-        http_req_duration: ['p(90)<1500'], // 90% of requests must complete below 1.5s
+        http_req_duration: ['p(90)<1500']
     },
+
+    stages: [
+        { target: 10, duration: '5s'},
+        { target: 30, duration: '10s'},
+        { target: 30, duration: '10s' },
+        { target: 0, duration: '5s' },
+    ]
 }
 
 export default function(){
     let responce = http.get('https://todomvc.com/examples/vue/')
-    
-    const check = check(responce, {
+
+    const check1 = check(responce, {
             'is responce status is 200 :' : (r)=> r.status === 200,
         })
-
-    sleep(1);
- 
-    errorRate.add(!check);
+    errorRate.add(!check1);
 }
